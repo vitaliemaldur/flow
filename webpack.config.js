b62b49f652a7e52ed7506a84e93cb4e3bdf52b2c
Vitalie Maldur <maldur.vitalie@gmail.com>
2020-04-01 12:29:43 +0300

    Added Preact

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,43 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './background-scripts/background.js',
+  entry: {
+    'background':  './src/background-scripts/background.js',
+    'browser-action': './src/browser-action/index.jsx'
+  },
   watch: true,
   output: {
-    filename: 'background.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [
+            ['@babel/plugin-transform-react-jsx', { pragma: 'h' }]
+          ],
+        }
+      }
     ]
   },
   plugins: [
     new CopyPlugin([
-      { from: 'manifest.json', to: 'manifest.json' },
       { from: 'node_modules/webextension-polyfill/dist/browser-polyfill.js' },
-      { from: 'icons', to: 'icons' }
+      { from: 'src/manifest.json', to: 'manifest.json' },
+      { from: 'src/assets/icons', to: 'icons' },
+      { from: 'src/browser-action/index.html', to: 'browser-action.html'  }
     ]),
   ],
+  resolve: {
+    alias: {
+      "react": "preact-compat",
+      "react-dom": "preact-compat"
+    },
+    extensions: ['.js', '.jsx']
+  },
 };
