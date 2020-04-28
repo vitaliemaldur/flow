051f0ed45bb55db688138460e8a45312767f271a
Vitalie Maldur <maldur.vitalie@gmail.com>
2020-04-28 11:32:59 +0300

    Centered image in blocked page

--- src/components/BlockedPage.jsx
+++ src/components/BlockedPage.jsx
@@ -3,7 +3,7 @@ import { h } from 'preact';
 export default function BrowserAction() {
   return (
     <div id="app-root">
-      <main className="container">
+      <main className="container mx-auto">
         <div className="flex justify-center">
           <img src="https://dummyimage.com/600x400/f20e56/fff.jpg&text=This+website+is+blocked" alt="blocked website" />
         </div>
